7d49225b90ae271f6377590e9adcf2da251012aa
Shai Petel <shai@petel.com>
2024-11-01 16:39:40 -0400

    fix dropdown loses value

--- src/controls/dropdown.tsx
+++ src/controls/dropdown.tsx
@@ -1,5 +1,5 @@
 import { Dropdown, DropdownProps, Option } from '@fluentui/react-components';
-import { firstOrNull, isNullOrUndefined } from '@kwiz/common';
+import { filterEmptyEntries, firstOrNull, isNullOrUndefined } from '@kwiz/common';
 import React from 'react';
 
 type ForwardProps = Omit<DropdownProps, "onSelect" | "selectedOptions" | "clearable">;
@@ -20,12 +20,21 @@ interface IProps<dataType, keyType extends string = string> extends ForwardProps
 }
 
 function $DropdownEX<keyType extends string = string, dataType = never>(props: IProps<dataType, keyType>, ref: React.ForwardedRef<HTMLButtonElement>) {
-
     const selected: keyType[] = Array.isArray(props.selected) ? props.selected : isNullOrUndefined(props.selected) ? [] : [props.selected];
+
+    //sometimes control will lose value when re-rendered
+    //use case: public forms when editing other fields after the dropdown was set
+    //re-set the text value manually to fix
+    let text = Array.isArray(props.selected)
+        ? filterEmptyEntries(props.selected.map(s => {
+            let v = firstOrNull(props.items, i => i.key === s);
+            return v ? v.value : ''
+        })).join(', ')
+        : props.selected || "";
 
     return (
         <Dropdown {...{ ...props, onSelect: undefined }} ref={ref} clearable={!props.required && !props.multiselect}
-            appearance='underline' selectedOptions={selected} onOptionSelect={(e, data) => {
+            appearance='underline' selectedOptions={selected} value={text} onOptionSelect={(e, data) => {
                 let o = firstOrNull(props.items, i => i.key === data.optionValue);
                 if (props.multiselect) {
                     let current = data.selectedOptions.map(s => firstOrNull(props.items, i => i.key === s));
