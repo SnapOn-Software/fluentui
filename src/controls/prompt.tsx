1a199f7cb2da53749dd7610e85a6e93388081fd4
Shai Petel <shai@petel.com>
2024-11-08 09:57:49 -0500

    expanded prompter control

--- src/controls/prompt.tsx
+++ src/controls/prompt.tsx
@@ -1,5 +1,5 @@
 import { Dialog, DialogActions, DialogBody, DialogContent, DialogSurface, DialogTitle, DialogTrigger } from '@fluentui/react-components';
-import { isNullOrEmptyString } from '@kwiz/common';
+import { isNotEmptyArray, isNullOrEmptyString } from '@kwiz/common';
 import React from 'react';
 import { useKWIZFluentContext } from '../helpers/context';
 import { ButtonEXProps, ButtonEXSecondary } from './button';
@@ -12,12 +12,27 @@ export interface IPrompterProps {
     onCancel?: () => void;
     okButtonText?: string;
     cancelButtonText?: string;
-    title?: string;
+    title?: string | JSX.Element;
     okButtonProps?: Partial<ButtonEXProps>;
     cancelButtonProps?: Partial<ButtonEXProps>;
     children?: JSX.Element;
+
+    /** allow to epand the dialog to be wider */
+    maxWidth?: number | string;
+
+    /** additional button actions at the bottom */
+    actions?: JSX.Element[];
+
+    /** dialog title action (send multiple ones inside <></>) */
+    titleAction?: JSX.Element;
+
+    /** specify a specific mount node for this dialog */
+    mountNode?: HTMLElement | null | {
+        element?: HTMLElement | null;
+        className?: string;
+    }
 }
-export const Prompter: React.FunctionComponent<React.PropsWithChildren<IPrompterProps>> = (props) => {
+export const Prompter = React.forwardRef<HTMLDivElement, (IPrompterProps)>((props, ref) => {
     const ctx = useKWIZFluentContext();
 
     let okProps: ButtonEXProps = {
@@ -39,24 +54,34 @@ export const Prompter: React.FunctionComponent<React.PropsWithChildren<IPrompter
         document.addEventListener("keydown", handler);
         return () => document.removeEventListener("keydown", handler);
     });
+
+    const actions: JSX.Element[] = [];
+    if (!props.hideOk) actions.push(<DialogTrigger key='ok' disableButtonEnhancement>
+        <ButtonEXSecondary {...okProps} />
+    </DialogTrigger>);
+    if (!props.hideCancel) actions.push(<DialogTrigger key='cancel' disableButtonEnhancement>
+        <ButtonEXSecondary {...cancelProps} />
+    </DialogTrigger>);
+    if (isNotEmptyArray(props.actions))
+        actions.push(...props.actions);
+
+
     return (
         <Dialog open>
-            <DialogSurface mountNode={ctx.mountNode}>
-                {!isNullOrEmptyString(props.title) && <DialogTitle>{props.title}</DialogTitle>}
+            <DialogSurface mountNode={props.mountNode || ctx.mountNode}
+                style={!isNullOrEmptyString(props.maxWidth) ? { maxWidth: props.maxWidth } : undefined}>
+                {!isNullOrEmptyString(props.title) && <DialogTitle
+                    action={props.titleAction}
+                >{props.title}</DialogTitle>}
                 <DialogBody>
-                    <DialogContent>
+                    <DialogContent ref={ref}>
                         {props.children}
                     </DialogContent>
-                    <DialogActions>
-                        {props.hideOk ? undefined : <DialogTrigger disableButtonEnhancement>
-                            <ButtonEXSecondary {...okProps} />
-                        </DialogTrigger>}
-                        {props.hideCancel ? undefined : <DialogTrigger disableButtonEnhancement>
-                            <ButtonEXSecondary {...cancelProps} />
-                        </DialogTrigger>}
-                    </DialogActions>
+                    {isNotEmptyArray(actions) && <DialogActions fluid={actions.length > 2}>
+                        {actions}
+                    </DialogActions>}
                 </DialogBody>
             </DialogSurface>
         </Dialog>
     );
-}
+});
