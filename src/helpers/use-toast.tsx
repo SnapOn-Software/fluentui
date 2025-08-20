41c46373f338155f911c815d75e7c1fb761b1889
Shai Petel <shai@petel.com>
2025-08-20 14:02:45 -0400

    typed toast dispatcher

--- src/helpers/use-toast.tsx
+++ src/helpers/use-toast.tsx
@@ -2,20 +2,24 @@ import { Link, Toast, ToastBody, Toaster, ToastFooter, ToastIntent, ToastTitle, 
 import { isNotEmptyArray } from "@kwiz/common";
 import { useKWIZFluentContext } from "./context-internal";
 
-export function useToast() {
+export type toastDispatcher = (info: {
+    title?: string;
+    body?: string;
+    subtitle?: string;
+    titleAction?: { text: string, onClick: () => void },
+    footerActions?: { text: string, onClick: () => void }[],
+    intent?: ToastIntent
+}) => void;
+export function useToast(): {
+    control: JSX.Element;
+    dispatch: toastDispatcher;
+} {
     const ctx = useKWIZFluentContext();
     const toasterId = useId("toaster");
     const { dispatchToast } = useToastController(toasterId);
     return {
         control: <Toaster mountNode={ctx.mountNode} toasterId={toasterId} />,
-        dispatch: (info: {
-            title?: string;
-            body?: string;
-            subtitle?: string;
-            titleAction?: { text: string, onClick: () => void },
-            footerActions?: { text: string, onClick: () => void }[],
-            intent?: ToastIntent
-        }) => {
+        dispatch: info => {
             dispatchToast(<Toast>
                 {info.title && <ToastTitle action={info.titleAction ? <Link onClick={info.titleAction.onClick}>{info.titleAction.text}</Link> : undefined}>{info.title}</ToastTitle>}
                 {info.body && <ToastBody subtitle={info.subtitle}>{info.body}</ToastBody>}
