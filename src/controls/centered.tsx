63c45126f4d4afb7d4f74c961e4f0b1c509115f9
Shai Petel <shai@petel.com>
2025-09-12 17:25:37 -0400

    added css to centered

--- src/controls/centered.tsx
+++ src/controls/centered.tsx
@@ -3,13 +3,15 @@ import { Horizontal } from './horizontal';
 import { Vertical } from './vertical';
 
 interface IProps {
+    css?: string[];
 }
-export const Centered: React.FunctionComponent<React.PropsWithChildren<IProps>> = (props) => {
+
+export const Centered = React.forwardRef<HTMLDivElement, React.PropsWithChildren<IProps>>((props, ref) => {
     return (
-        <Vertical main vCentered>
+        <Vertical main vCentered css={props.css} ref={ref}>
             <Horizontal hCentered>
                 {props.children}
             </Horizontal>
         </Vertical>
     );
-}
+});
