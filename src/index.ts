1b6052fe1d09aaed0c4a34c39bdfcad3af20b9b8
Shai Petel <shai@petel.com>
2025-09-16 19:16:57 -0400

    remove test methods

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,3 @@
-import { CommonConfig, GetLogger } from '@kwiz/common';
-
 export * from './controls';
 export * from './helpers';
 export * from './styles';
-
-const logger = new GetLogger("fluentui");
-export function printInfo() {
-    logger.i.log(CommonConfig.i);
-}
