60dc3ee7302b9be035904ed4cb598ffbd9ed2694
Shai Petel <shai@petel.com>
2025-10-24 14:17:54 -0400

    updated loading animation

--- src/controls/loading.tsx
+++ src/controls/loading.tsx
@@ -1,11 +1,18 @@
-import { LOGO_ANIM_SMALL } from '@kwiz/common';
-import { Centered } from './centered';
+import { LOGO_ANIM_DARK, LOGO_ANIM_LIGHT } from '@kwiz/common';
 import React from 'react';
+import { useKWIZFluentContext } from '../helpers/context-internal';
+import { Centered } from './centered';
 
 interface IProps {
+    dark?: boolean;
 }
 export const Loading: React.FunctionComponent<IProps> = (props) => {
+    const ctx = useKWIZFluentContext();
+    const src = props.dark === true || ctx.dark
+        ? LOGO_ANIM_DARK
+        : LOGO_ANIM_LIGHT;
+
     return (
-        <Centered><img src={LOGO_ANIM_SMALL} alt="loading" style={{ width: '15vw' }} /></Centered>
+        <Centered><img src={src} alt="loading" style={{ width: '15vw' }} /></Centered>
     );
 }
