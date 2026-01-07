98eee0effcddd0ed6f5c9c8f6fdafee5c26e3d80
Shai Petel <shai@petel.com>
2026-01-07 12:40:08 -0500

    added export

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -5,6 +5,7 @@ export * from './hooks';
 export * from './hooks-events';
 export * from './use-alerts';
 export * from './use-editable-control';
+export * from './use-highlight';
 export * from './use-theme';
 export * from './use-toast';
 
