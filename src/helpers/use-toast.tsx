323efa18a55c1b0eaee053f1659eef15220a9f2b
Shai Petel <shai@petel.com>
2026-02-27 12:53:13 -0500

    toast options

--- src/helpers/use-toast.tsx
+++ src/helpers/use-toast.tsx
@@ -1,6 +1,42 @@
-import { Link, Spinner, Toast, ToastBody, Toaster, ToastFooter, ToastIntent, ToastTitle, useId, useToastController } from "@fluentui/react-components";
+import { Link, Spinner, Toast, ToastBody, Toaster, ToastFooter, ToastId, ToastIntent, ToastPoliteness, ToastPosition, ToastTitle, useId, useToastController } from "@fluentui/react-components";
 import { isNotEmptyArray } from "@kwiz/common";
 import { useKWIZFluentContext } from "./context-internal";
+
+
+export type toastOptionsType = {
+    /**
+ * Uniquely identifies a toast, used for update and dismiss operations
+ */
+    toastId: ToastId;
+    /**
+     * The position the toast should render to
+     */
+    position?: ToastPosition;
+    /**
+     * Auto dismiss timeout in milliseconds
+     * @default 3000
+     */
+    timeout?: number;
+    /**
+     * Toast timeout pauses while focus is on another window
+     * @default false
+     */
+    pauseOnWindowBlur?: boolean;
+    /**
+     * Toast timeout pauses while user cursor is on the toast
+     * @default false
+     */
+    pauseOnHover?: boolean;
+    /**
+     * Higher priority toasts will be rendered before lower priority toasts
+     */
+    priority?: number;
+    /**
+     * Used to determine [aria-live](https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/ARIA_Live_Regions) narration
+     * This will override the intent prop
+     */
+    politeness?: ToastPoliteness;
+}
 
 export type toastDispatcherType = (info: {
     title?: string;
@@ -12,18 +48,20 @@ export type toastDispatcherType = (info: {
     intent?: ToastIntent | "progress";
     media?: JSX.Element;
     inverted?: boolean;
-
-}) => void;
+} & Partial<toastOptionsType>) => void;
 export function useToast(): {
     control: JSX.Element;
     dispatch: toastDispatcherType;
+    dismiss: (toastId: ToastId) => void;
+    dismissAll: () => void;
+    update: (info: toastOptionsType) => void;
 } {
     const ctx = useKWIZFluentContext();
     const toasterId = useId("toaster");
-    const { dispatchToast } = useToastController(toasterId);
+    const { dispatchToast, dismissToast, dismissAllToasts, updateToast } = useToastController(toasterId);
     return {
         control: <Toaster mountNode={ctx.mountNode} toasterId={toasterId} />,
-        dispatch: info => {
+        dispatch: (info) => {
             dispatchToast(<Toast appearance={info.inverted ? "inverted" : undefined}>
                 {info.title && <ToastTitle
                     media={info.media
@@ -40,11 +78,16 @@ export function useToast(): {
                     </ToastFooter>
                 }
             </Toast>, {
+                ...(info || {}),
                 intent:
                     info.intent === "progress"
                         ? "info"
                         : info.intent || "info"
+
             });
-        }
+        },
+        dismiss: dismissToast,
+        dismissAll: dismissAllToasts,
+        update: info => updateToast(info)
     }
 }
