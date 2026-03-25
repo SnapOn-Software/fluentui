a768236090e582f33cbb3852a53df38dce4b4964
Shai Petel <shai@petel.com>
2026-03-25 10:54:29 -0400

    fix auto focus

--- src/helpers/use-auto-focus-ex.tsx
+++ src/helpers/use-auto-focus-ex.tsx
@@ -1,18 +1,16 @@
-import { isNullOrUndefined } from "@kwiz/common";
 import { useEffect } from "react";
 import { useRefWithState } from "./hooks";
 
 /** better auto-focus that actually works. use the ref on the element you wish to focus.
  * focus set initially, and then again on a timeout - default is 200ms timeout. send 0 to skip the timeout
  */
-export function useAutoFocusEX({ timeout }: { timeout?: number }) {
+export function useAutoFocusEX({ timeout }: { timeout?: number } = { timeout: 200 }) {
     const focusableRef = useRefWithState<HTMLElement>();
     useEffect(() => {
         if (focusableRef.ref.current) {
-            const useTimeout = isNullOrUndefined(timeout) || timeout > 0;
             focusableRef.ref.current.focus();
-            if (useTimeout)
-                window.setTimeout(() => focusableRef.ref.current.focus(), timeout > 0 ? timeout : 200);
+            if (timeout > 0)
+                window.setTimeout(() => focusableRef.ref.current.focus(), timeout);
         }
     }, [focusableRef.value, timeout]);
     return { set: focusableRef.set }
